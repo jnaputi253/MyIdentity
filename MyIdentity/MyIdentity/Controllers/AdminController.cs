f3ad0867580e93a155823e55f5e863c48d6563a9
Naputi, Juan <jnaputi@PAML.com>
2017-03-20 16:05:44 -0700

    Added admin controller and Index view

---
+++ MyIdentity/MyIdentity/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyIdentity.Models;
+
+namespace MyIdentity.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index() => View(_userManager.Users);
+
+        public IActionResult Create()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
